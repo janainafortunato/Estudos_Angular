15e24654942ac6b7e72ee21907c849435358fb87
janainafortunato <janaina.projetos@live.com>
2021-08-02 00:00:34 -0300

    criação da tabela de produtos/botões de edit, delete

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 
+
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
@@ -9,14 +10,14 @@ import { ProductService } from '../product.service';
 })
 export class ProductReadComponent implements OnInit {
 
-  products: Product[] | undefined
+  products!: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action']
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
-      console.log(products)
     })
 
   }
