2c40c8f58650d3249d172d738d59de504a47c957
janainafortunato <janaina.projetos@live.com>
2021-08-03 23:44:14 -0300

    criação do update e delete

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -13,20 +13,36 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient ) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      verticalPosition: "top",
+      panelClass: isError ?  ['msg-error'] : ['msg-success']
+    });
   }
 
   create(product: Product):Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product);
   }
 
   read(): Observable<Product[]> {
-      return this.http.get<Product[]>(this.baseUrl)
+      return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  readById(id: string): Observable<Product>{
+      const url = `${this.baseUrl}/${id}`
+      return this.http.get<Product>(url);
+  }
+
+  update(product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product);
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 
 }
